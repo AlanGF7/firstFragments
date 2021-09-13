6ce455d3442695eddc1ccf929aa4765961d39405
AlanGF7 <af189107@gmail.com>
2021-09-13 11:49:03 -0600

    Vistas terminadas
    Ya se han copiado, ahora falta poder navegar entre ellas

---
+++ app/src/main/java/com/cupelli/practicafragmentsimages/detailFragment.kt
@@ -0,0 +1,17 @@
+package com.cupelli.practicafragmentsimages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class detailFragment : Fragment(R.layout.fragment_detail) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/cupelli/practicafragmentsimages/middleFragment.kt
@@ -0,0 +1,17 @@
+package com.cupelli.practicafragmentsimages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class middleFragment : Fragment(R.layout.fragment_middle) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
